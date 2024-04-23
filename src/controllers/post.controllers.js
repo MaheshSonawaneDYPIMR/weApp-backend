711c08cbacc8f6035b96387e32c0a509c620be35
mahesh <147mahesh258@gmail.com>
2024-04-23 16:58:27 +0530

    (^_^) Sad...

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -7,8 +7,8 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { Post } from "../models/post.models.js";
 
 const publishPost = asyncHandler(async (req, res) => {
-  const { postMsg } = req.body;
-  const postPicFileLocalPath = req.file.path ;
+  const { postMsg,postPic } = req.body;
+  const postPicFileLocalPath = postPic.uri ;
   let postPicture = null;
   let postMessage = null;
  console.log("local path hhhhh",postPicFileLocalPath);
